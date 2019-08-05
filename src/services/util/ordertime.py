a0b4b2cf8d806642b242df639984deb59871da6f
Nianmin Guo <zenmai0822@live.com>
2019-08-04 23:24:14 -0400

    added util class of parsing date strings

---
+++ src/services/util/ordertime.py
@@ -0,0 +1,53 @@
+from datetime import date, datetime, timedelta
+import pytz
+
+
+class Ordertime:
+
+    @classmethod
+    def _parse24h(cls, string):
+        try:
+            return datetime.strptime(string, "%I:%M").time()
+        except ValueError:
+            return None
+
+    @classmethod
+    def _parse12h(cls, string):
+        try:
+            return datetime.strptime(string, "%H:%M").time()
+        except ValueError:
+            return None
+
+    @classmethod
+    def _parse12h_ampm(cls, string):
+        try:
+            return datetime.strptime(string, "%H:%M %p").time()
+        except ValueError:
+            return None
+
+    @classmethod
+    def parse_time(cls, string, tzstring="America/New_York"):
+        input_time = cls._parse24h(string) or cls._parse12h(string) or cls._parse12h_ampm(string)
+        if not input_time:
+            raise ValueError("Unrecognized date format {}, accept format is 'hh:mm [am/pm]'".format(string))
+        result_dttm_naive = datetime.combine(date.today(), input_time)
+        timezone = pytz.timezone(tzstring)
+        return timezone.localize(result_dttm_naive)
+
+    @classmethod
+    def parse_interval(cls, amount, unit, tzstring="America/New_York"):
+        amount = int(amount)
+        time_now = datetime.now()
+        delta = None
+        if unit in {"hours", "hour", "h"}:
+            delta = timedelta(hours=amount)
+        elif unit in {"mins", "min", "m"}:
+            delta = timedelta(minutes=amount)
+        elif unit in {"secs", "sec", "s"}:
+            delta = timedelta(seconds=amount)
+        else:
+            raise ValueError("Unrecognized time unit format {}, accept format is [h]our(s)/[m]inute(s)/[s]econd(s)")
+
+        result_dttm_naive = time_now + delta
+        timezone = pytz.timezone(tzstring)
+        return timezone.localize(result_dttm_naive)
